8cc6801d78a03794cc44201a72b1258b5692b6ab
jlb1999 <jlbroughton88@gmail.com>
2019-10-06 06:43:02 -0400

    accidentally forgot to save .gitignore

---
+++ client/build/precache-manifest.a7a16219cdf77fa6662dd4b858bc16f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b63ea88cbcd8423b8df22f8142c2c7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd907d0ce78070077e57",
+    "url": "/static/css/main.786162e5.chunk.css"
+  },
+  {
+    "revision": "94b3c1e7c18d6cfd8a90",
+    "url": "/static/js/2.b70bcd56.chunk.js"
+  },
+  {
+    "revision": "dd907d0ce78070077e57",
+    "url": "/static/js/main.1c077154.chunk.js"
+  },
+  {
+    "revision": "f1c6019deaec6f4a9d0b",
+    "url": "/static/js/runtime-main.11bc70d3.js"
+  }
+]);
