279207fdb7ab3a9b8b09a18225c34d0ce597fd02
jlb1999 <jlbroughton88@gmail.com>
2019-10-06 06:22:13 -0400

    added heroku path, deploying

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require("express");
-const fetch = require("node-fetch")
+const path = require("path");
 const app = express();
 
 app.get("/", (req, res) => {
     res.send("Hello from server")
 })
 
+app.use(express.static(path.join(_dirname, "./client/build")))
+
 app.use("/", require("./routes.js"))
 
-app.listen(process.env.PORT || "5003", () => {
+app.listen(process.env.PORT || 5003, () => {
     console.log("App listening on port 5003")
 })
