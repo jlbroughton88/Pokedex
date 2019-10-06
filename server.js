30c5b67266b431d6aff0c296dd82f2f0576a9f85
jlb1999 <jlbroughton88@gmail.com>
2019-10-06 06:40:52 -0400

    changed .gitignore and new build

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/server", (req, res) => {
     res.send("Hello from server")
 })
 
