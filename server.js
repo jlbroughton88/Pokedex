21f0f3e055bdcb6cd6a8d846a758d4da2970001b
jlb1999 <jlbroughton88@gmail.com>
2019-10-08 11:05:20 -0400

    new route format for server

--- server.js
+++ server.js
@@ -2,13 +2,26 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.get("/server", (req, res) => {
-    res.send("Hello from server")
-})
+// app.get("/server", (req, res) => {
+//     res.send("Hello from server")
+// })
 
-app.use(express.static(path.join(__dirname, "/client/build")))
+// app.use(express.static(path.join(__dirname, "/client/build")))
 
-app.use("/", require("./routes.js"))
+// app.use("/", require("./routes.js"))
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("/*", (req, res) => {
+      res.sendFile(path.join(__dirname, "./client/build/index.html"))
+    })
+  } else {
+    app.use(express.static(path.join(__dirname, "/client/public")));
+    app.get("/*", (req, res) => {
+      res.sendFile(path.join(__dirname, "./client/public/index.html"))
+    })
+  }
+  
 
 app.listen(process.env.PORT || 5003, () => {
     console.log("App listening on port 5003")
